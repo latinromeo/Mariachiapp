a79d370122ab768506103a952f42ee9230b48f2a
Mariachi en Santo Domingo <MariachisRD@gmail.com>
2025-06-22 05:39:51 +0000

    agrega este logo por favor

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 
 export function Logo() {
   return (
-    <div className="flex items-center justify-center py-2">
+    <div className="flex items-center justify-center py-2 px-4">
       <Image
         src="/logo.png"
         alt="Mariachi Reyes de México Logo"
-        width={68}
-        height={80}
+        width={180}
+        height={60}
         priority
+        className="object-contain"
       />
     </div>
   )
