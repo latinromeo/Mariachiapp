fd32db52f68f55294d4e6ce4410684fc4acc1618
Mariachi en Santo Domingo <MariachisRD@gmail.com>
2025-06-24 01:56:12 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,8 +7,8 @@ export function Logo() {
       <Image
         src="/logo.svg"
         alt="Logo de Mariachi Reyes de México"
-        width={150}
-        height={50}
+        width={180}
+        height={60}
         priority
         className="object-contain"
       />
